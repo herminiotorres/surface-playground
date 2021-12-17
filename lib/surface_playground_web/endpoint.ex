f88b84ca1dbb8ea0e4477ad447a542c7093333fd
Herminio Torres <herminiocesar@gmail.com>
2021-12-17 18:33:35 -0300

    Use production config from Heroku ENV variables and decrease socket timeout (#8)
    fix always heroku rebuild
    
    This has to do with stale dependencies which are not getting recompiled properly. It's possible to force Heroku to recompile all dependencies on each deploy, which should fix this problem. The way to do it is to add a new file called elixir_buildpack.config at the root of the application. The file should contain this line:
    
    ```
    always_rebuild=true
    ```
    
    Commit this file to the repository and try to push again to Heroku.
    
    bump elixir and erlang/otp version
    
    remove the websocket timeout
    
    prepares for heroku
    
    more prepares for heroku
    
    fix prepares for heroku

--- lib/surface_playground_web/endpoint.ex
+++ lib/surface_playground_web/endpoint.ex
@@ -10,7 +10,7 @@ defmodule SurfacePlaygroundWeb.Endpoint do
     signing_salt: "S/HP8CGh"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [timeout: 45_000, connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
